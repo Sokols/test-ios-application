480ab3b98fd72938e9a1e6a18c03fb43e91b029f
Igor <yetiak99@gmail.com>
2024-06-29 08:39:27 +0200

    Initial commit

---
+++ SalesmanApp/SalesmanAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalesmanAppApp.swift
+//  SalesmanApp
+//
+//  Created by Igor SOKÓŁ on 29/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SalesmanAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
